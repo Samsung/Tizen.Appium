f63cc74476171c59c2da27ecc268c324b2778ebd
RinaYou <rina6350.you@samsung.com>
2018-02-08 15:21:22 +0900

    Add the script of EditorTest

---
+++ Appium.UITests/TC/EditorTest1.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Tizen;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Interactions.Internal;
+
+namespace Appium.UITests
+{
+    [TestFixture("Tizen")]
+    public class EditorTest1
+    {
+        string PlatformName;
+        AppiumDriver Driver;
+
+        public EditorTest1(string platform)
+        {
+            PlatformName = platform;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Driver = new AppiumDriver(PlatformName);
+            FormsTizenGalleryUtils.FindTC(Driver, this.GetType().Name);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void GetTextTest()
+        {
+            string result = WebElementUtils.GetText(Driver, "editor");
+            Assert.AreEqual("Plase, input any sentence.", result);
+        }
+
+        void GetTextTest2()
+        {
+            string origin = "This test is for testing Editor with very long text. This software is the confidential and proprietary information of Samsung Electronics, Inc. You shall not disclose such Confidential Information and shall use it only in accordance with the terms of the license agreement you entered into with Samsung.";
+            string result = WebElementUtils.GetText(Driver, "internalEditor");
+            Assert.AreEqual(origin, result);
+        }
+    }
+}
---
+++ Appium.UITests/TC/EditorTest2.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Tizen;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Interactions.Internal;
+
+namespace Appium.UITests
+{
+    [TestFixture("Tizen")]
+    public class EditorTest2
+    {
+        string PlatformName;
+        AppiumDriver Driver;
+
+        public EditorTest2(string platform)
+        {
+            PlatformName = platform;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Driver = new AppiumDriver(PlatformName);
+            FormsTizenGalleryUtils.FindTC(Driver, this.GetType().Name);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void SetTextTest()
+        {
+            string origin = WebElementUtils.GetText(Driver, "longEditor");
+            string add = "abcdefg";
+            WebElementUtils.SetText(Driver, "longEditor", add);
+
+            WebElementUtils.Click(Driver, "editor");
+            string result = WebElementUtils.GetText(Driver, "longEditor");
+            Assert.AreEqual(origin+add, result);
+        }
+
+        [Test]
+        public void CompletedTest()
+        {
+            WebElementUtils.SetText(Driver, "longEditor", "ABC");
+            WebElementUtils.Click(Driver, "editor");
+            string result = WebElementUtils.GetText(Driver, "editor");
+            Assert.AreEqual("Editing completed", result);
+        }
+    }
+}
