ca76ab13558722cd66feaddb4c7770e8bd00e77a
RinaYou <rina6350.you@samsung.com>
2018-02-12 17:09:35 +0900

    Add the timeout procedure about invalid click

--- UIAutomator/src/server/server.cpp
+++ UIAutomator/src/server/server.cpp
@@ -34,6 +34,7 @@
 #include <gio/gio.h>
 #include <Ecore.h>
 #include <Ecore_Con.h>
+#include <Elementary.h>
 
 #include "utils/dbus_utils.h"
 #include "utils/log.h"
@@ -46,6 +47,8 @@
 using namespace std;
 
 const int SERVER_TIMEOUT = 30000;
+const double SUBSCRIBE_TIMEOUT = 5;
+
 
 Server::Server()
 {
@@ -56,6 +59,7 @@ Server::Server()
 void Server::AddHandler(string action, CommandHandler function)
 {
     HandlerMap[action] = std::move(function);
+    SubscribeTimer = 0;
 }
 
 Server::~Server()
@@ -118,6 +122,15 @@ string Server::GetRequestId()
 void Server::SetAppSocket(Ecore_Con_Client* socket)
 {
     Appium = socket;
+}
+
+Eina_Bool SubscribeTimeout(void* data)
+{
+    _D("Subscribe Timeout");
+    Server::getInstance().DeleteTimer();
+    string reply = JsonUtils::ActionReply(false);
+    Server::getInstance().SendMessageToAppium(reply);
+    return EINA_FALSE;
 }
 
 Eina_Bool ClientAddCallback(void *data EINA_UNUSED, int type EINA_UNUSED, Ecore_Con_Event_Client_Add *ev)
@@ -290,6 +303,8 @@ void Server::ClickHandler(char* buf)
             int X = ElementGetIntMessage(request.AutomationId, "GetCenterX");
             int Y = ElementGetIntMessage(request.AutomationId, "GetCenterY");
             touch.Click(X, Y);
+
+            SubscribeTimer = ecore_timer_loop_add(SUBSCRIBE_TIMEOUT, SubscribeTimeout, 0);
         }
         else
         {
@@ -602,7 +617,7 @@ void Server::PressHardWareKeyHandler(char* buf)
 
 void Server::ShutDownHandler()
 {
-    _D("Enter");
+    _D("Shutdown");
     string reply = JsonUtils::ActionReply("Shutdown");
     SendMessageToAppium(reply);
 }
@@ -610,6 +625,8 @@ void Server::ShutDownHandler()
 void Server::EventHandler(void *data, DBusMessage *msg)
 {
     _D("Enter");
+    DeleteTimer();
+
     DBusMessageIter args;
     char* elementId = NULL;
     char* eventName = NULL;
@@ -627,6 +644,15 @@ void Server::EventHandler(void *data, DBusMessage *msg)
     string reply = JsonUtils::ActionReply(true);
     SendMessageToAppium(reply);
     return;
+}
+
+void Server::DeleteTimer()
+{
+    if(SubscribeTimer != 0){
+        ecore_timer_del(SubscribeTimer);
+        SubscribeTimer = 0;
+        _D("Timer deleted");
+    }    
 }
 
 void Server::init()
