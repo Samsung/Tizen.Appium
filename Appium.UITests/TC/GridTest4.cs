ec8908e277197de4da8eb4f09c80e2c0258cce6c
RinaYou <rina6350.you@samsung.com>
2018-02-12 21:12:54 +0900

    Add TCs for Grid

---
+++ Appium.UITests/TC/GridTest4.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Tizen;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Interactions.Internal;
+using System.Drawing;
+
+namespace Appium.UITests
+{
+    [TestFixture("Tizen")]
+    public class GridTest4
+    {
+        string PlatformName;
+        AppiumDriver Driver;
+
+        public GridTest4(string platform)
+        {
+            PlatformName = platform;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Driver = new AppiumDriver(PlatformName);
+            FormsTizenGalleryUtils.FindTC(Driver, this.GetType().Name);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void LocationTest()
+        {
+            Point point = WebElementUtils.GetLocation(Driver, "button1");
+            Assert.AreEqual(360, point.X);
+            Assert.AreEqual(199, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "button2");
+            Assert.AreEqual(296, point.X);
+            Assert.AreEqual(510, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "button3");
+            Assert.AreEqual(647, point.X);
+            Assert.AreEqual(510, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "button4");
+            Assert.AreEqual(296, point.X);
+            Assert.AreEqual(1025, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "button5");
+            Assert.AreEqual(647, point.X);
+            Assert.AreEqual(1025, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "button6");
+            Assert.AreEqual(711, point.X);
+            Assert.AreEqual(767, point.Y);
+        }
+
+        [Test]
+        public void SizeTest()
+        {
+            Size area = WebElementUtils.GetSize(Driver, "button1");
+            Assert.AreEqual(50, area.Height);
+            Assert.AreEqual(364, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "button2");
+            Assert.AreEqual(258, area.Height);
+            Assert.AreEqual(300, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "button3");
+            Assert.AreEqual(258, area.Height);
+            Assert.AreEqual(50, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "button4");
+            Assert.AreEqual(258, area.Height);
+            Assert.AreEqual(300, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "button5");
+            Assert.AreEqual(258, area.Height);
+            Assert.AreEqual(50, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "button6");
+            Assert.AreEqual(519, area.Height);
+            Assert.AreEqual(8, area.Width);
+        }
+    }
+}
---
+++ Appium.UITests/TC/GridTest5.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Tizen;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Interactions.Internal;
+using System.Drawing;
+
+namespace Appium.UITests
+{
+    [TestFixture("Tizen")]
+    public class GridTest5
+    {
+        string PlatformName;
+        AppiumDriver Driver;
+
+        public GridTest5(string platform)
+        {
+            PlatformName = platform;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Driver = new AppiumDriver(PlatformName);
+            FormsTizenGalleryUtils.FindTC(Driver, this.GetType().Name);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void LableLocationTest()
+        {
+            Point point = WebElementUtils.GetLocation(Driver, "label1");
+            Assert.AreEqual(358, point.X);
+            Assert.AreEqual(216, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "label2");
+            Assert.AreEqual(108, point.X);
+            Assert.AreEqual(273, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "label3");
+            Assert.AreEqual(349, point.X);
+            Assert.AreEqual(683, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "label4");
+            Assert.AreEqual(602, point.X);
+            Assert.AreEqual(657, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "label5");
+            Assert.AreEqual(255, point.X);
+            Assert.AreEqual(1172, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "label6");
+            Assert.AreEqual(602, point.X);
+            Assert.AreEqual(1172, point.Y);
+        }
+
+        [Test]
+        public void BoxLocationTest()
+        {
+            Point point = WebElementUtils.GetLocation(Driver, "BoxView1");
+            Assert.AreEqual(349, point.X);
+            Assert.AreEqual(273, point.Y);
+
+            point = WebElementUtils.GetLocation(Driver, "BoxView2");
+            Assert.AreEqual(108, point.X);
+            Assert.AreEqual(683, point.Y);
+        }
+
+        [Test]
+        public void LabelSizeTest()
+        {
+            Size area = WebElementUtils.GetSize(Driver, "label1");
+            Assert.AreEqual(26, area.Height);
+            Assert.AreEqual(36, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "label2");
+            Assert.AreEqual(19, area.Height);
+            Assert.AreEqual(89, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "label3");
+            Assert.AreEqual(383, area.Height);
+            Assert.AreEqual(143, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "label4");
+            Assert.AreEqual(408, area.Height);
+            Assert.AreEqual(100, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "label5");
+            Assert.AreEqual(100, area.Height);
+            Assert.AreEqual(238, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "label6");
+            Assert.AreEqual(100, area.Height);
+            Assert.AreEqual(100, area.Width);
+        }
+
+        [Test]
+        public void BoxSizeTest()
+        {
+            Size area = WebElementUtils.GetSize(Driver, "BoxView1");
+            Assert.AreEqual(19, area.Height);
+            Assert.AreEqual(143, area.Width);
+
+            area = WebElementUtils.GetSize(Driver, "BoxView2");
+            Assert.AreEqual(383, area.Height);
+            Assert.AreEqual(89, area.Width);
+        }
+    }
+}
