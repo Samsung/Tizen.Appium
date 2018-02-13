e0ea404ba1c2981e04d9843c7f3b81c679cb17c4
RinaYou <rina6350.you@samsung.com>
2018-02-13 19:50:45 +0900

    Add TCs for BackgroundColor

---
+++ Appium.UITests/TC/BackgroundColorTest.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Tizen;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace Appium.UITests
+{
+    [TestFixture(FormsTizenGalleryUtils.Platform)]
+    public class BackgroundColorTest
+    {
+        string PlatformName;
+        AppiumDriver Driver;
+
+        public BackgroundColorTest(string platform)
+        {
+            PlatformName = platform;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Driver = new AppiumDriver(PlatformName);
+            FormsTizenGalleryUtils.FindTC(Driver, this.GetType().Name);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void ClickTest()
+        {
+            Point point = WebElementUtils.GetLocation(Driver, "imgTest");
+            WebElementUtils.Click(Driver, "btnStartAnim");
+            Point point2 = WebElementUtils.GetLocation(Driver, "imgTest");
+            Assert.AreNotEqual(point, point2);
+        }
+    }
+}
