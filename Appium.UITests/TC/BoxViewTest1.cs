52d0696876c36694b7358219447688b0505f32f9
RinaYou <rina6350.you@samsung.com>
2018-02-18 18:07:46 +0900

    Add TCs for checking BoxView

---
+++ Appium.UITests/TC/BoxViewTest1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Tizen;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace Appium.UITests
+{
+    [TestFixture(FormsTizenGalleryUtils.Platform)]
+    public class BoxViewTest1
+    {
+        string PlatformName;
+        AppiumDriver Driver;
+
+        public BoxViewTest1(string platform)
+        {
+            PlatformName = platform;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Driver = new AppiumDriver(PlatformName);
+            FormsTizenGalleryUtils.FindTC(Driver, this.GetType().Name);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void PositionTest1()
+        {
+            Point expect = new Point(360, 288);
+            Point pt = WebElementUtils.GetLocation(Driver, "BoxView1");
+            Assert.AreEqual(expect, pt);
+        }
+
+        [Test]
+        public void PositionTest2()
+        {
+            Point expect = new Point(360, 387);
+            Point pt = WebElementUtils.GetLocation(Driver, "BoxView2");
+            Assert.AreEqual(expect, pt);
+        }
+
+        [Test]
+        public void PositionTest3()
+        {
+            Point expect = new Point(360, 485);
+            Point pt = WebElementUtils.GetLocation(Driver, "BoxView3");
+            Assert.AreEqual(expect, pt);
+        }
+    }
+}
---
+++ Appium.UITests/TC/BoxViewTest3.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Tizen;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using System.Drawing;
+
+namespace Appium.UITests
+{
+    [TestFixture(FormsTizenGalleryUtils.Platform)]
+    public class BoxViewTest3
+    {
+        string PlatformName;
+        AppiumDriver Driver;
+
+        public BoxViewTest3(string platform)
+        {
+            PlatformName = platform;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Driver = new AppiumDriver(PlatformName);
+            FormsTizenGalleryUtils.FindTC(Driver, this.GetType().Name);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void ColorTest1()
+        {
+            string ret = WebElementUtils.GetAttribute(Driver, "colorBox", "Color");
+            var touch = new RemoteTouchScreenUtils(Driver);
+            touch.Down(647, 512);
+            touch.Up(647, 512);
+            string ret2 = WebElementUtils.GetAttribute(Driver, "colorBox", "Color");
+        }
+
+        [Test]
+        public void ColorTest2()
+        {
+            string ret = WebElementUtils.GetAttribute(Driver, "preColorBox", "Color");
+            var touch = new RemoteTouchScreenUtils(Driver);
+            touch.Down(662, 748);
+            touch.Up(662, 748);
+            string ret2 = WebElementUtils.GetAttribute(Driver, "preColorBox", "Color");
+        }
+
+        [Test]
+        public void ColorTest3()
+        {
+            string ret = WebElementUtils.GetAttribute(Driver, "colorBox", "Color");
+            var touch = new RemoteTouchScreenUtils(Driver);
+            touch.Down(119, 628);
+            touch.Up(119, 628);
+            string ret2 = WebElementUtils.GetAttribute(Driver, "colorBox", "Color");
+        }
+
+        [Test]
+        public void ColorTest4()
+        {
+            string ret = WebElementUtils.GetAttribute(Driver, "preColorBox", "Color");
+            var touch = new RemoteTouchScreenUtils(Driver);
+            touch.Down(101, 847);
+            touch.Up(101, 847);
+            string ret2 = WebElementUtils.GetAttribute(Driver, "preColorBox", "Color");
+        }
+    }
+}
