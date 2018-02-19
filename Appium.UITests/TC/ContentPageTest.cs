478b010bda193411f87eeb42d07c2dc2ffcbaa68
RinaYou <rina6350.you@samsung.com>
2018-02-19 18:51:13 +0900

    Add TCs for checking ContentPage and CarouselPage

---
+++ Appium.UITests/TC/ContentPageTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using OpenQA.Selenium.Appium;
+using OpenQA.Selenium.Appium.Tizen;
+using OpenQA.Selenium.Appium.Android;
+using OpenQA.Selenium.Remote;
+using NUnit.Framework;
+using OpenQA.Selenium.Appium.MultiTouch;
+using OpenQA.Selenium.Interactions.Internal;
+
+namespace Appium.UITests
+{
+    [TestFixture(FormsTizenGalleryUtils.Platform)]
+    public class ContentPageTest
+    {
+        string PlatformName;
+        AppiumDriver Driver;
+
+        public ContentPageTest(string platform)
+        {
+            PlatformName = platform;
+        }
+
+        [TestFixtureSetUp]
+        public void Setup()
+        {
+            Driver = new AppiumDriver(PlatformName);
+            FormsTizenGalleryUtils.FindTC(Driver, this.GetType().Name);
+        }
+
+        [TestFixtureTearDown]
+        public void TearDown()
+        {
+            Driver.Quit();
+        }
+
+        [Test]
+        public void ContentTest()
+        {
+            WebElementUtils.Click(Driver, "one");
+            WebElementUtils.Click(Driver, "two");
+            string ret = WebElementUtils.GetAttribute(Driver, "one", "Text");
+            Assert.AreEqual("ONE", ret);
+        }
+    }
+}
