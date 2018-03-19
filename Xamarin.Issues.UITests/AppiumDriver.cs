1afcdc86f1a30e5bb831d98961a387d332769fb2
RinaYou <rina6350.you@samsung.com>
2018-03-19 10:55:08 +0900

    Support the teamcity on Xamarin.Issues.UITests

--- Xamarin.Issues.UITests/AppiumDriver.cs
+++ Xamarin.Issues.UITests/AppiumDriver.cs
@@ -51,8 +51,7 @@ namespace Xamarin.Issues.UITests
             capabillities.SetCapability("platformName", "Tizen");
             capabillities.SetCapability("appPackage", "org.tizen.xamarin.forms.issues.tizen");
             capabillities.SetCapability("app", "Xamarin.Forms.Tizen.Issues-1.0.0.tpk");
-
-            var driver = new TizenDriver<AppiumWebElement>(new Uri("http://10.113.111.163:8080/wd/hub"), capabillities);
+            var driver = new TizenDriver<AppiumWebElement>(new Uri("http://192.168.0.49:8080/wd/hub"), capabillities);
             return driver;
         }
 
@@ -63,7 +62,6 @@ namespace Xamarin.Issues.UITests
             capabillities.SetCapability("platformName", "Android");
             capabillities.SetCapability("appPackage", "AppiumTest.Android");
             capabillities.SetCapability("appActivity", "md5fb91949aa2c8850087e612420184ba95.MainActivity");
-
             var driver = new AndroidDriver<AppiumWebElement>(new Uri("http://127.0.0.1:4723/wd/hub"), capabillities, TimeSpan.FromMinutes(5));
             return driver;
         }
